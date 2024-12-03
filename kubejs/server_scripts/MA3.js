b1446eea3a19d7eab16656d7f1e389ef8f2c4900
MidnightPigeon <chen_xs_021105@126.com>
2024-12-03 21:40:55 +0800

    several update

--- kubejs/server_scripts/MA3.js
+++ kubejs/server_scripts/MA3.js
@@ -1,1 +1,9 @@
 //MA3相关修改在这里编写
+ServerEvents.recipes(event => {
+    
+    //替换温特姆符法魔杖顶端
+    event.replaceInput({mod:'mna',output:'mna:manaweaver_wand'},
+        'mna:vinteum_dust' , 'mna:transmuted_silver')
+
+
+})
