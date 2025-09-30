ef7306bf1e97a15bb3f7f5ede0211b3b42be2347
MidnightPigeon <Midnight_Pigeon@outlook.com>
2025-09-30 23:29:15 +0800

    several updates
    总之是更新。

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WizardryCraft
  MineCraft Modpack for wizards!
+ 
+ 目前仍在制作，暂时无法用于游玩。
--- kubejs/server_scripts/EL.js
+++ kubejs/server_scripts/EL.js
@@ -1,25 +1,13 @@
 //神秘遗物相关修改在这里编写
 ServerEvents.recipes(event => {
     
-    //无主护身符
-    event.shaped(Item.of('enigmaticlegacy:unwitnessed_amulet',1),
-        ['ABA',
-        'BCB',
-        'ABA'],
-        {
-            A:'mna:decoration/transmuted_silver_block',
-            B:'mna:purified_vinteum_ingot',
-            C:'enigmaticlegacy:insignia'
-        }
-    )
-
     //飞升护符（七合一）
     event.shaped(Item.of('enigmaticlegacy:ascension_amulet',1),
         ['AAA',
         'AHA',
         'AIA'],
         {
-            A:'enigmaticlegacy:unwitnessed_amulet',
+            A:'enigmaticlegacy:enigmatic_amulet',
             H:'minecraft:netherite_ingot',
             I:'enigmaticlegacy:astral_block'
         }
