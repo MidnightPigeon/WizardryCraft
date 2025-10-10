640a8afaa340ef8523c8504221f212b91622254f
MidnightPigeon <Midnight_Pigeon@outlook.com>
2025-10-11 01:50:59 +0800

    transmuted silver fix
    增加液体浇筑材料性质。

--- kubejs/server_scripts/recipes/TCMaterials.js
+++ kubejs/server_scripts/recipes/TCMaterials.js
@@ -1,6 +1,19 @@
 //匠魂材料注册辅助配方写在这里
 ServerEvents.recipes(event => {
 
+    function MaterialMelting(material, fluid, fluid_amount, temperature) {
+        event.custom({
+            "type": "tconstruct:material_fluid",
+            "fluid": {
+                "amount": fluid_amount,
+                "fluid": fluid
+            },
+            "output": material,
+            "temperature": temperature
+        })
+    }
+
+
     //嬗变银
     event.custom({
         "type": "tconstruct:material",
@@ -11,6 +24,6 @@ ServerEvents.recipes(event => {
         "needed": 1,
         "value": 1
     })
-
+    MaterialMelting("kubejs:transmuted_silver","kubejs:liquid_transmuted_silver",90,750)
 
 })
