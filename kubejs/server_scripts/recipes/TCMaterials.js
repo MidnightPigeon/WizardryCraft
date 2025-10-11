9cad05c730e5ac1dc254879802b3512a121ad77c
MidnightPigeon <Midnight_Pigeon@outlook.com>
2025-10-11 12:27:56 +0800

    transmuted silver update
    完成较为彻底的自定义。
    特性仍需额外处理，后面再说。
    定义了各类用于处理材料的函数。

--- kubejs/server_scripts/recipes/TCMaterials.js
+++ kubejs/server_scripts/recipes/TCMaterials.js
@@ -1,6 +1,7 @@
 //匠魂材料注册辅助配方写在这里
 ServerEvents.recipes(event => {
 
+    //液体材料定义
     function MaterialMelting(material, fluid, fluid_amount, temperature) {
         event.custom({
             "type": "tconstruct:material_fluid",
@@ -10,6 +11,31 @@ ServerEvents.recipes(event => {
             },
             "output": material,
             "temperature": temperature
+        })
+    }
+    //超越1的材料定义
+    function OverandReduce(OverItem, Item, Material, OverValue) {
+        event.custom({
+            "type": "tconstruct:material",
+            "ingredient": {
+                "item": OverItem
+            },
+            "material": Material,
+            "needed": 1,
+            "value": OverValue
+        })
+        event.custom({
+            "type": "tconstruct:material",
+            "ingredient": {
+                "item": OverItem
+            },
+            "leftover": {
+                "count": 1,
+                "item": Item
+            },
+            "material": Material,
+            "needed": 1,
+            "value": OverValue
         })
     }
 
@@ -24,6 +50,7 @@ ServerEvents.recipes(event => {
         "needed": 1,
         "value": 1
     })
-    MaterialMelting("kubejs:transmuted_silver","kubejs:liquid_transmuted_silver",90,750)
+    MaterialMelting("kubejs:transmuted_silver","kubejs:liquid_transmuted_silver",90,500)
+    OverandReduce("mna:decoration/transmuted_silver_block","mna:transmuted_silver","kubejs:transmuted_silver",9)
 
 })
